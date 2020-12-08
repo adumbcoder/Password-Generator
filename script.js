a066e057c0580c93a7498490d3495707517879ab
Joshua Pickens <pickensprogramming@gmail.com>
2020-12-07 21:45:05 -0500

    restarted project. kept UI build and rewrote funstions.

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
 // Assignment Code
 let generateBtn = document.querySelector('#generate');
-let yesLowerCase = document.getElementById('lowerCase');
-let yesUpperCase = document.getElementById('upperCase');
-let yesNumeric = document.getElementById('numeric');
-let yesSpecialChar = document.getElementById('specialChar');
-let errorMessage = document.getElementById('errorMsg')
+let yesLowerCase = document.getElementById('#lowerCase');
+let yesUpperCase = document.getElementById('#upperCase');
+let yesNumeric = document.getElementById('#numeric');
+let yesSpecialChar = document.getElementById('#specialChar');
+let errorMessage = document.getElementById('#errorMsg')
 let passwordOptions = '';
-// arrays
-let lettersLower = ['a b c d e f g h i j k l m n o p q r s t u v w x y z'];
-let lettersUpper = ['A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'];
-let numericChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-let specChar =  ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '<', '>', '/', '?'];
 
-//Make sure a checkbox is selected/add selection to the password maker
-function addPWOptions(){
-  if (!lettersLower && !lettersUpper && !numericChar && !specChar){
-    errorMessage.textContent = 'You must select at least one option.';
+// data
+let lettersLower = 'a b c d e f g h i j k l m n o p q r s t u v w x y z';
+let lettersUpper = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
+let numericChar = '1 2 3 4 5 6 7 8 9 0';
+let specChar =  '! @ # $ %  ^ & * ( ) - = _ + < > ? / : ; ';
+
+// call password function
+generateBtn.addEventListener ('click', writePassword());
+
+// Get options 
+
+function getOptions(){
+  if (!yesLowerCase.checked && !yesUpperCase.checked && !yesNumeric.checked && !yesSpecialChar.checked){
+    errorMessage = "You must select at least one option";
   }
-  if (yesLowerCase === true) {
-    passwordOptions += lettersLower.split('');
+  if (yesLowerCase.checked) {
+    passwordOptions += lettersLower;
   }
-  if(yesUpperCase === true) {
-    passwordOptions +=lettersUpper.split(' ');
+  if (yesUpperCase.checked) {
+    passwordOptions += lettersUpper;
   }
-  if(yesNumeric === true) {
+  if (yesNumeric.checked) {
     passwordOptions += numericChar;
   }
-  if(yesSpecialChar === true) {
-    passwordOptions += specChar;
+  if (yesSpecialChar.checked) {
+    passwordOptions +=specChar;
   }
   return passwordOptions;
 }
 
-// Write password to the #password input
-function writePassword() {
-  let password = addPWOptions();
-  let passwordText = document.querySelector('#password');
-  let finalPassword = '';
+function buildPassword(length) {
+	let passString = getOptions();
+	let passLength = passString.length;
 
-  for (let i = 0, )
+	let result = '';
 
-  passwordText.value = finalPassword;
+	for ( let i = 0; i < length; i++ ) {
+		result += passString.charAt(Math.floor(Math.random() * passLength));
+	}
 
+	return result;
 }
 
-// Add event listener to generate button
+// make sure length is selected
+function writePassword() {
+  if (!passwordLength) {
+    errorMessage.innerHTML = "You must select a Password length.";
+  }
+}
 
-generateBtn.addEventListener('click', writePassword);
+
+
 
