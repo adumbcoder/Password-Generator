4d1783d5f6a12e04cb8c82f163b2cf130071ae00
Joshua Pickens <pickensprogramming@gmail.com>
2020-12-08 12:12:18 -0500

    fixed a scope issue, finished comment out the script file.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let yesSpecialChar = document.getElementById('specialChar');
 let errorMsgOptions = document.getElementById('errorMsgOptions');
 let errorMsgLen = document.getElementById('errorMsgLen');
 let passInput = document.getElementById('password');
-let passwordOptions = '';
+
 
 // data
 let lettersLower = 'abcdefghijklmnopqrstuvwxyz';
@@ -15,43 +15,50 @@ let lettersUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let numericChar = '1234567890';
 let specChar =  '!@#$%^&*()-=_+<>?/:;';
 
+//store users length input
 function getuserlenInput(passLen) {
   return passLen.value;
 }
 
 // Get options 
 function getOptions(){
+  let passwordOptions = '';
+  // if no options are selected give an error msg
+  
   if (!yesLowerCase.checked && !yesUpperCase.checked && !yesNumeric.checked && !yesSpecialChar.checked){
     errorMsgOptions.setAttribute('aria-hidden', false);
     errorMsgOptions.textContent = 'You must select at least one option';
   }
+  //if passlen is empty display a error
+  
   if (!passLen.value) {
     errorMsgLen.setAttribute('aria-hidden', false);
     errorMsgLen.textContent = 'You must select a length.';
-    
+  //clears the error msg if they pick a length
   }else {
     errorMsgLen.setAttribute('aria-hidden', true);
     errorMsgLen.textContent = '';
     
   }
+  // if lowercase is wanted adds to passwordOptions/clears error msg
   if (yesLowerCase.checked) {
     passwordOptions += lettersLower;
     errorMsgOptions.setAttribute('aria-hidden', true);
     errorMsgOptions.textContent = '';
   }
-  
+  // if uppercase is wanted adds to passwordOptions/clears error msg
   if (yesUpperCase.checked) {
     passwordOptions += lettersUpper;
     errorMsgOptions.setAttribute('aria-hidden', true);
     errorMsgOptions.textContent = '';
   }
-    
-  
+  // if numeric is wanted adds to passwordOptions/clears error msg
   if (yesNumeric.checked) {
     passwordOptions += numericChar;
     errorMsgOptions.setAttribute('aria-hidden', true);
     errorMsgOptions.textContent = '';
   }
+  // if special chars are wanted adds to passwordOptions/clears error msg
   if (yesSpecialChar.checked) {
     passwordOptions += specChar;
     errorMsgOptions.setAttribute('aria-hidden', true);
@@ -63,7 +70,7 @@ function getOptions(){
 }
 
 
-// Call getOtions and pass in the options that the user selected. 
+// Call getOptions and pass in the options that the user selected. 
 // 
 function startPass(length) {
 	let passString = getOptions();
@@ -77,14 +84,13 @@ function startPass(length) {
   
   return randPass;
 }
- 
+ //onclick takes startPass and gets users length input to make the final password
+ //that will be displayed to the user.
 generateBtn.addEventListener('click', function(){
   let finalPassword = startPass(getuserlenInput(passLen));
   if (finalPassword){
     passInput.innerHTML = finalPassword;
-    
   }
-  
 });
 
 
